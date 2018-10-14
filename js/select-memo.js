efda5cadc783848f087404721a1a56dbd4b216c0
okillgus <oliver.killgus@googlemail.com>
2018-10-14 08:39:22 +0200

    select option and save

--- js/select-memo.js
+++ js/select-memo.js
@@ -16,8 +16,8 @@ define(function(require) {
       console.log('calling initData.');
       this.initData();
       console.log('calling importData.');
-      var data = this.importData();
-      this.updateModel(data);
+      //var data = this.importData();
+      //this.updateModel(data);
     },
 
     postRender: function() {
@@ -43,7 +43,7 @@ define(function(require) {
 
     importData: function(){
       // Daten in die View laden
-      var _data = this.readDB() || {};
+      var _data = this.readDB();
       _data = this.checkData(_data);
       console.log('imported:');
       console.log(_data);
@@ -52,18 +52,19 @@ define(function(require) {
     },
 
     checkData: function(data){
-      var _storageName = this.model.get('storageName');
-      var _db = this.model.get(_storageName);
       var _topic = this.model.get('topic');
-      if (!_db){
-        _db = {};
-        _db[_topic] = [];
+      if (!data){
+        data = {};
+        data[_topic] = [];
+        this.writeDB(data);
       }
-      else if (!_db.hasProperty(_topic)){
-        _db[_topic] = [];
+      else if (!data.hasProperty(_topic)){
+        data[_topic] = [];
+        this.writeDB(data);
       }
-      this.model.set(_storageName, _db);
-      return _db;
+      console.log('checked:');
+      console.log(data);
+      return data;
     },
 
     updateModel: function(data){
@@ -76,79 +77,54 @@ define(function(require) {
 
     updateView: function(data){
 
-      console.log('marking up options');
       var _topic = this.model.get('topic');
+      var _visit = this.model.get('step');
+
       for (var item in data[_topic]){
+        console.log('updating item');
         console.log(item);
         // graphische Darstellung: schon gewählt wurde...  
-        var _classes = item.steps.join(' ');
-        this.markup(item, _classes);
+        // var _classes = item.steps.join(' ');
+        var _inputCont = $('#item_'+item.inputId);
+        // class_str hinzufügen
+        _inputCont.addClass(item.steps);
+
+        if (item.steps.indexOf(_visit) > -1){
+          var _inputElem = $('#'+item.inputId);
+          _inputElem.prop('checked', true);
+        }        
       }
     },
     
-    markup: function(obj, class_str){
-      // class_str und _visit sind für einen Aufruf dasselbe, z.B. "fst"
-      var _inputCont = $('#item_'+obj.inputId);
-      _inputCont.setAttribute('class', 'component-item select-memo-item'+class_str);
-      // Korrektur: aktuell gewählt?
-      var _visit = this.model.get('step');
-      if (class_str.indexOf(_visit) > -1){  // d.h. z.B. beim ersten Besuch ist "fst" in "fst, trd" schon enthalten.
-        var _inputElem = $('#'+obj.inputId);
-        _inputElem.checked = true;
-      }
-    },
-
     toggleSelect: function(ev){
-      var _visit = this.model.get('step');
-      var _topic = this.model.get('topic');
-      var _items = this.model.get('items');
-      // console.log(_visit);
-      // console.log('Changed Elem, inputId: '+ev.currentTarget.id, 'container_'+ev.currentTarget.id);
-      //input, label add class: 'highlighted selected a11y-selected' -> gewählt
-      // if obj is selected: unselect + un-markup
 
-      // prüfen!
-      // -> Klassen je nach Schritt hinzufügen oder wegnehmen.
-      // jQuery-Context!
-      var obj = $("#"+ev.currentTarget.id);
-      var _inputId = obj.prop("id");
+      var _visit = this.model.get('step');
+      var _inputId = ev.currentTarget.id;
       var _inputCont = $('#item_'+_inputId);
-      var _classes = _inputCont.prop('class');  
-      if (obj.prop("checked")){
-        if(_classes.substr(-1) != " "){ _visit = " "+_visit; }
-        _classes = _classes + _visit;
-        _items = this.setItem(_items, _inputId);
-      }
-      else{// else: select + markup
-        _classes = _classes.replace(_visit, '');
-        _items = this.removeItem(_items, _inputId);
-      }
-      _inputCont.prop('class', _classes);
-      //var _visit = this.model.get('step');
-      console.log("_classes, str: ", _classes, ", obj: ", _inputCont.prop('class'));
-      // -> update model
-      this.model.set('items', _items);
-      // -> write db.
+
+      var changedItem = this.toggleItem(_inputId, _visit);
+      _inputCont.toggleClass(_visit);
       this.saveData();
     },
 
-    setItem: function(list, id, cls){
-      for (var n = 0; n < list.length; n++){
-        if (list[n].inputId == id && !(cls in list[n].steps)) {
-          list[n].steps.push(cls);
-        }
-      }
-      return list;
-    },
+    toggleItem: function(id, cls){
+      var _items = this.model.get('items');
 
-    removeItem: function(list, id, cls){
-      for (var n = 0; n < list.length; n++){
-        if (list[n].inputId == id && cls in list[n].steps){
-          var idx = list[n].steps.indexOf(cls);
-          list[n].steps.splice(idx, 1);
+      for (var n = 0; n <_items.length; n++){
+        var item = _items[n];
+        if (item.inputId == id) {
+          if (item.steps.indexOf(cls) < 0){
+            if (item.steps.substr(-1) != " "){cls = " "+ cls;}
+            item.steps = item.steps + cls;
+          }
+          else{
+            item.steps = item.steps.replace(new RegExp(cls, 'g'), "");
+          }
+          _items[n] = item;
+          this.model.set('items', _items);
+          return item;   
         }
       }
-      return list;
     },
 
     saveData: function(){
@@ -202,7 +178,8 @@ define(function(require) {
     readDB: function(){
       // Daten aus localStorage holen
       var _data = localStorage.getItem(this.model.get('storageName'));
-      console.log('reading DB ...', _data);
+      console.log('reading DB:');
+      console.log(_data);
       if (!_data) { return false; }
       return JSON.parse(_data); 
     },
